58d70888e353e4ea53efa0023d646149804d215d
Christian Gamperl <christian@gamperl.me>
2025-03-01 09:38:39 +0100

    Initial commit

---
+++ app/src/main/java/at/wifi/swdev/qrcodescanner/MainActivity.java
@@ -0,0 +1,60 @@
+package at.wifi.swdev.qrcodescanner;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityResultLauncher<String> launcher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            granted -> {
+                // Hat der Benutzer den Zugriff erlaubt bzw. die Berechtigung erteilt?
+                if (granted) {
+                    // Ja, hat er
+                    startCamera();
+                } else {
+                    // Nein, hat nicht...
+                    // -> wir dürfen nicht auf die Kamera zugreifen und können daher kein Barcodes erkennen...
+                }
+            }
+    );
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        // Prüfen, ob Berechtigung für Kamera vorhanden
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            // Wir haben die Berechtigung bereits
+            startCamera();
+        } else {
+            // Wir die Berechtigung noch nicht --> Benutzer darum bitten
+            launcher.launch(Manifest.permission.CAMERA);
+        }
+
+
+    }
+
+    private void startCamera() {
+
+    }
+}
